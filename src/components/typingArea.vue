8bfc44925a34e76b34819155a6974165068006e6
myix765 <myix765@gmail.com>
2024-03-08 18:38:55 -0500

    added accuracy checking functionality, separated lineNums and snippets, edited styling

--- src/components/typingArea.vue
+++ src/components/typingArea.vue
@@ -1,22 +1,66 @@
 <template>
     <div>
-        <TypingTest
-            :currentCodeSnippet="currentCodeSnippet"
-        ></TypingTest>
+        <div id="typing-area">
+            <LineNums
+                id="line-nums"
+                :snippetRows="snippetRows"
+            >
+            </LineNums>
+            <TypingTest
+                :currentCodeSnippet="currentCodeSnippet"
+                :snippetCols="snippetCols"
+                :snippetRows="snippetRows"
+            ></TypingTest>
+        </div>
     </div>
 </template>
 
 <script>
 import TypingTest from './typingTest.vue'
+import LineNums from './lineNums.vue'
 
 export default {
     props: ['currentCodeSnippet'],
     components: {
         TypingTest,
+        LineNums,
     },
+    data() {
+        return {
+            snippetCols: 0,
+            snippetRows: 0,
+        }
+    },
+    created() {
+        this.getColAndRow(this.currentCodeSnippet)
+    },
+    methods: {
+        getColAndRow(snippet) {
+            let maxLength = 0
+            let snippetArray = snippet.replace(/\t/g, '    ').split("\n")
+            for (let line of snippetArray) {
+                if (line.length > maxLength) {
+                    maxLength = line.length
+                }
+            }
+            this.snippetRows = snippetArray.length + 1 /* +1 for if user incorrectly types newline */
+            this.snippetCols = maxLength + 1 /* +1 for if user incorrectly types newline */
+        },
+    }
 }
 </script>
 
 <style scoped>
+    #typing-area {
+        display: flex;
+        height: 100%;
+        width: 100%;
+    }
 
+    #line-nums {
+        font-family: inherit;
+        color: var(--gray);
+        width: 3ch;
+        height: 100%;
+    }
 </style>
